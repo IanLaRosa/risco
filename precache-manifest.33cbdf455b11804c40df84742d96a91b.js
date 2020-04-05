58e3c71f59a17557e211758d8f3b907d23a7f2f0
IanLaRosa <ian.rosa@usp.br>
2020-04-05 19:03:25 -0300

    Updates

---
+++ precache-manifest.33cbdf455b11804c40df84742d96a91b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2e6c4511796ce7a19ac76f5011fff6",
+    "url": "/risco/index.html"
+  },
+  {
+    "revision": "0928a4798bbb5bd0352f",
+    "url": "/risco/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "55d2dcba84ad5b421762",
+    "url": "/risco/static/js/2.298d67a9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/risco/static/js/2.298d67a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0928a4798bbb5bd0352f",
+    "url": "/risco/static/js/main.0da37a1c.chunk.js"
+  },
+  {
+    "revision": "47e9e037f42453a512d3",
+    "url": "/risco/static/js/runtime-main.4a465cc0.js"
+  }
+]);
