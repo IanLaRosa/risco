6710a07c74b1c7032b45cc588fdb8df81e093fbf
IanLaRosa <ian.rosa@usp.br>
2020-04-12 20:23:08 -0300

    Charts fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import { Home } from './Home'
+import  Home  from './Home'
 import { ActivePlane } from './ActivePlane'
 import { NoMatch } from './NoMatch'
 import { Layout } from './components/Layout'
