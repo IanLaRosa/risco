c887428df1c6c338117cd46e9ee2e1d444c36a0c
IanLaRosa <ian.rosa@usp.br>
2020-04-12 21:07:21 -0300

    Jumbotron

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -19,7 +19,7 @@ const Styles = styled.div`
 export const NavigationBar = () =>(
     <Styles>
         <Navbar expand="lg">
-            <Navbar.Brand href="/risco/">Home</Navbar.Brand>
+            <Navbar.Brand href="/risco/">Flight Analysis</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
