a53b65eddebca334e6a0f9af1fe68d00ce73773d
IanLaRosa <ian.rosa@usp.br>
2020-04-14 01:59:46 -0300

    gets

--- src/Home.js
+++ src/Home.js
@@ -4,14 +4,24 @@ import { Line } from 'react-chartjs-2'
 const Home = () => {
     const [chartData, setChartData] = useState({})
     const [chartBData, setChartBData] = useState({})
+    const [dados, setDados] = useState({})
+    const [eixox, setEixox] = useState({})
+
+    useEffect(() =>{
+        fetch("http://localhost:5000/iluminatti").then(response => response.json().then(data => {setDados(data)}))
+    },[])
+
+    useEffect(() =>{
+        fetch("http://localhost:5000/time").then(response => response.json().then(data => {setEixox(data)}))
+    },[])
 
     const chart = () => {
         setChartData({
-            labels: ['a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'],
+            labels: eixox,
             datasets: [
                 {
                     label: 'energia',
-                    data: [123, 3, 32, 10, 22, 3, 32, 10, 22, 3, 32, 10, 22, 3, 32, 10, 22, 3, 32, 10, 22, 3, 32, 10, 22, 3, 32, 10, 22, 3, 32, 10, 22, 3, 32, 10, 22],
+                    data: dados,
                     backgroundColor: [
                         'rgba(75, 192, 192, 0.6)'
                     ],
@@ -45,15 +55,19 @@ const Home = () => {
         <div className="App">
             <h1>Lorem</h1>
             <div style={{height: "600px", width:"800px"}}>
-                <Line data={chartData} options={{
-                    responsive: true
-                }}/>
+                {Object.keys(chartData).length &&
+                    <Line data={chartData} options={{
+                        responsive: true
+                    }}/>
+                }
             </div>
             <h1>Lorem2</h1>
             <div style={{height: "600px", width:"800px"}}>
-                <Line data={chartBData} options={{
-                    responsive: true
-                }}/>
+                {Object.keys(chartBData).length &&
+                    <Line data={chartBData} options={{
+                        responsive: true
+                    }}/>
+                }
             </div>
         </div>
     )
